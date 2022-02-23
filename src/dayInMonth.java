3f5756023d1677b25ef6fb139b8fdc76db3be99d
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-02-24 00:30:11 +0200

    q

--- src/dayInMonth.java
+++ src/dayInMonth.java
@@ -5,16 +5,35 @@ public class dayInMonth {
     public static void main(String[] args) {
         System.out.println("Введите год и месяц, я вам скажу скольк дней в месяце было.");
         Calendar myCalendar = (Calendar) Calendar.getInstance().clone();
-        int your_year;
-        int your_month;
+        int year, month;
+        String monthName = "";
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите год: ");
-        your_year = input.nextInt();
-        System.out.print("Введите месяц: ");
-        your_month = input.nextInt();
-        myCalendar.set(your_year, your_month, -1);
-        int max_date = myCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-        System.out.print("Количество дней в месяце: " + max_date);
+        System.out.print("Ввод года: ");
+        year = input.nextInt();
+        System.out.print("Введите номер месяца: ");
+        month = input.nextInt();
+        myCalendar.set(year, month, -1);
+        switch (month) {
+            case 1 -> monthName = ("Январе");
+            case 2 -> monthName = ("Феврале");
+            case 3 -> monthName = ("Марте");
+            case 4 -> monthName = ("Апреле");
+            case 5 -> monthName = ("Мае");
+            case 6 -> monthName = ("Июне");
+            case 7 -> monthName = ("Июле");
+            case 8 -> monthName = ("Августе");
+            case 9 -> monthName = ("Сентябре");
+            case 10 -> monthName = ("Октябре");
+            case 11 -> monthName = ("Ноябре");
+            case 12 -> monthName = ("Декабре");
+        }
+        if (year <= 0 || month <= 0) {
+            System.out.println("К сожалению, не смогу вам посчитать, год или месяц не может быть >= 0");
+        } else {
+            int max_date = myCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+            System.out.print("В " + monthName + " " + year + " года " + max_date + " дней");
 
+        }
     }
 }
+
